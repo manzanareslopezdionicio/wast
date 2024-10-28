d501e1250ff11763871067b097adeec0514daa0d
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2024-10-28 14:03:19 -0600

    incio

--- appweb.py
+++ appweb.py
@@ -1,3 +1,31 @@
 import streamlit as st 
 
-st.title("Bienvenidos")
+home = st.Page(
+    page="vestas/home.py",
+    title="Inicio",
+    icon=":material/home:",
+   # icon="🏠",
+   default=True,
+)
+acerca_de_page= st.Page(
+    page="vistas/acerca_de.py",
+    title="Acerca de",
+    icon=":material/account_circle",
+)
+project_1 = st.Page(
+    page="vistas/ventas.py",
+    title="ventas",
+    icon="📊",
+)
+project_2 = st.Page(
+    page="vistas/chatbot.py",
+    title="Chatbot",
+    icon="🤖"
+)
+pg = st.navigation(
+    {
+        "Información:":[home, acerca_de_page],
+        "Proyectos:":[project_1,project_2],
+    }
+)
+pg.run()
