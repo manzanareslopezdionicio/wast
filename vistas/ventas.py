14d7284134dc388a64dbdc5f96a4868a3d987d7a
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2024-11-13 14:49:48 -0600

    time

--- vistas/ventas.py
+++ vistas/ventas.py
@@ -1,3 +1,15 @@
 import streamlit as st 
+import pandas as pd
+import plotly.express as px
+import datetime
 
-st.title("Ventas")
+st.subheader("Filtrar Datos y Captura de Datos")
+st.write("El procesamiento de datos a traves Ciencia de Datos usando Streamlit de Python")
+
+dfDatos = pd.read_csv('http://raw.githubusercontent.com/gcastano/datasets/main/gapminder_data.csv')
+
+#Mostrar tabla de los registros
+#dfDatos=dfDatos[(dfDatos[""])]
+
+st.metric("***Registros Totales***", len(dfDatos))
+st.dataframe(dfDatos, use_container_width=True)
